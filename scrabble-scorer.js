f08027932ba8a09a876e6f394a359df5c68115fc
Kyle Feller <76542408+Fe1ky@users.noreply.github.com>
2021-06-17 22:11:38 +0000

    finished all the steps! my scrabble scorer works as advertised (lets hope the autoGrader agrees)

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,26 +33,103 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+  let word = input.question("Let's play some scrabble! \n\nEnter a word to score:");
+  return word
+};
+ 
+
+
+// let simpleScore = function(word){
+//  return word.length
+// };
+
+// let vowelBonusScore = function(word){
+//   word = word.toUpperCase();
+//   console.log(word)
+//   let vowels = 0;
+//   let consonants = 0;
+
+//   for(let i = 0; i<word.length;i++){
+//      if(word[i] == 'A'|| word[i] == 'E'||word[i] == 'I'||word[i] == 'O'||word[i] == 'U'){
+//     vowels = vowels+3
+//       } else {
+//     consonants++
+//       }
+//     }
+// return vowels+consonants;
+// };
+
+let simpleScore = {
+  name:'Simple Score',
+  description:'Each letter is worth 1 point.',
+  scoreFunction: function(word){
+ return word.length
+}
+};
+let vowelBonusScore = {
+  name:'Bonus Vowels',
+  description:'	Vowels are 3 pts, consonants are 1 pt',
+  scoreFunction: function(word){
+  word = word.toUpperCase();
+  
+  let vowels = 0;
+  let consonants = 0;
+
+  for(let i = 0; i<word.length;i++){
+     if(word[i] == 'A'|| word[i] == 'E'||word[i] == 'I'||word[i] == 'O'||word[i] == 'U'){
+    vowels = vowels+3
+      } else {
+    consonants++
+      }
+    }
+return vowels+consonants;
+}
+};
+let classic = {
+  name:'Scrabble',
+  description:'The traditional scoring algorithm.',
+  scoreFunction: function(word){
+   let score = 0;
+   for(let i=0; i<word.length;i++){
+     score = score +newPointStructure[word[i].toUpperCase()]
+   }
+   return score
+  }
+ };
+
+
+function scorerPrompt() {
+   let scoringMethod = input.question("Which scoring algorithm would you like to use? \n \n 0 - Simple: One point per character \n 1 - Vowel Bonus: Vowels are worth 3 points \n 2 - Scrabble: Uses scrabble point system \n Enter 0, 1, or 2:");
+  console.log(`Score for '${word}' is ${scoringAlgorithms[scoringMethod].scoreFunction(word)}`);
+}
+
+
+
+
+let newPointStructure = transform(oldPointStructure);
+function transform(oldPoint) {
+  let tempObj = {};
+  	  for (const pointValue in oldPoint){  
+        for (let j=0; j<oldPoint[pointValue].length;j++ ){
+          tempObj[oldPoint[pointValue][j]] = Number(pointValue);
+        }  
+      }
+      return tempObj;
 };
 
-let simpleScore;
+ let scrabbleScore = function(word){
+   let score = 0;
+   for(let i=0; i<word.length;i++){
+     score = score +newPointStructure[word[i].toUpperCase()]
+   }
+   return score
+ };
+const scoringAlgorithms = [simpleScore, vowelBonusScore, classic ];
 
-let vowelBonusScore;
-
-let scrabbleScore;
-
-const scoringAlgorithms = [];
-
-function scorerPrompt() {}
-
-function transform() {};
-
-let newPointStructure;
-
+//console.log(scrabbleScorer('kyle'))
 function runProgram() {
-   initialPrompt();
-   
+   word = initialPrompt();
+   scorerPrompt(word);
 }
 
 // Don't write any code below this line //
